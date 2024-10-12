ea80972e226861651005bc3db6c885b18f7250f6
Carlos Penagos <carlos.penagos@monolegal.co>
2024-10-12 17:07:21 -0500

    Se corrigen algunas advertencias

--- src/main/java/com/uptc/frw/lafabrica/LaFabricaApplication.java
+++ src/main/java/com/uptc/frw/lafabrica/LaFabricaApplication.java
@@ -16,24 +16,4 @@ public class LaFabricaApplication {
         SpringApplication.run(LaFabricaApplication.class, args);
     }
 
-
-    @Bean
-    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener() {
-        return event -> {
-            String swaggerUrl = "http://localhost:8080/swagger-ui.html";
-
-            // Verifica si la aplicación se está ejecutando en un entorno gráfico
-            if (Desktop.isDesktopSupported()) {
-                try {
-                    // Intenta abrir el navegador
-                    Desktop.getDesktop().browse(new URI(swaggerUrl));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // Si no se puede abrir el navegador, notifica al usuario
-                System.out.println("La aplicación está corriendo. Abre el siguiente enlace para acceder a Swagger: " + swaggerUrl);
-            }
-        };
-    }
 }
--- src/main/java/com/uptc/frw/lafabrica/controller/HomeController.java
+++ src/main/java/com/uptc/frw/lafabrica/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.uptc.frw.lafabrica.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
